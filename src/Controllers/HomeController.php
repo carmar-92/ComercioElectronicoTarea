b48e6c25bf9aa08b34a74aa8e2400d90f12fe2e2
carmar-92 <157653987+carmar-92@users.noreply.github.com>
2024-10-26 21:25:19 -0600

    Cambios en product, HomeController, Parameters.env

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,12 +1,16 @@
 <?php
+
 namespace Controllers;
+
 use \Dao\Products\Products as ProductsDao;
 use \Views\Renderer as Renderer;
+use \Utilities\Site as Site;
 
 class HomeController extends PublicController
 {
-    public function run() :void
+    public function run(): void
     {
+        Site::addLink("public/css/products.css");
         $viewData = [];
         $viewData["productsOnSale"] = ProductsDao::getDailyDeals();
         $viewData["productsHighlighted"] = ProductsDao::getFeaturedProducts();
@@ -14,5 +18,4 @@ class HomeController extends PublicController
         Renderer::render("home", $viewData);
     }
 }
-
 ?>
